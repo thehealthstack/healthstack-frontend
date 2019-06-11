d7cbeb86290b3c96afd28e4009e0970b00e730e1
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-11 13:41:04 +0100

    create a footer component for welcome page

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,49 @@
+<template>
+  <footer class="footer has-background-black-ter">
+    <div class="columns">
+      <div class="column">
+        <img src="../assets/logo.png">
+      </div>
+      <div class="column">
+        <h3>Products</h3>
+        <ul>
+          <li>Delivery</li>
+          <li>Payment</li>
+          <li>Scheduling</li>
+        </ul>
+      </div>
+      <div class="column">
+        <h3>Company</h3>
+        <ul>
+          <li>About</li>
+          <li>Team</li>
+          <li>Careers</li>
+          <li>Customers</li>
+        </ul>
+      </div>
+      <div class="column">
+        <h3>Resource</h3>
+        <ul>
+          <li>Contact</li>
+          <li>Support</li>
+          <li>Terms</li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+
+<script>
+
+export default {
+  name: 'FooterComponent',
+};
+
+</script>
+
+
+<style>
+
+
+</style>
