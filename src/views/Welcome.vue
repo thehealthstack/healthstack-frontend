f9d299724121221a708b00fadad4afdc206c3269
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-11 13:48:15 +0100

    add the services, signup, navbar and footer components to this view

---
+++ src/views/Welcome.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="welcome">
+    <NavBarComponent/>
+    <SignupComponent/>
+    <ServicesComponent/>
+    <FooterComponent/>
+  </div>
+</template>
+
+<script>
+import NavBarComponent from '@/components/NavBarComponent.vue';
+import SignupComponent from '@/components/SignupComponent.vue';
+import ServicesComponent from '@/components/ServicesComponent.vue';
+import FooterComponent from '@/components/FooterComponent.vue';
+
+export default {
+  name: 'Welcome',
+  components: {
+    NavBarComponent,
+    SignupComponent,
+    ServicesComponent,
+    FooterComponent,
+  },
+};
+</script>
+
+
+<style scoped>
+
+</style>
