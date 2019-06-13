51d5cc3df95299fe2d5ddeca0f9f4974a91a1b23
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 14:51:32 +0100

    break view into sidebarnav, uploadbar and readmedia components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,60 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <NavBarComponent :isAuthenticated="isAuthenticated"/>
+    <div class="section">
+      <div class="columns">
+        <SideBarNavComponent />
+        <div class="column">
+          <UploadBarComponent />
+          <ResultMediaComponent v-for="(result, index) in results" :result="result" :key="index"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ResultMediaComponent from "@/components/ResultMediaComponent.vue";
+import UploadBarComponent from "@/components/UploadBarComponent.vue";
+import SideBarNavComponent from "@/components/SideBarNavComponent.vue"
+import NavBarComponent from "@/components/NavBarComponent.vue";
 
 export default {
-  name: 'home',
-  components: {
+  name: "Home",
+  data() {
+    return {
+      isAuthenticated: true,
+      results: [
+        {
+          telephone: "+237690346872",
+          email: "vladimirfomene@gmail.com",
+          fileUrls: [
+            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
+          ]
+        },
+        {
+          telephone: "+237690346872",
+          email: "vladimirfomene@gmail.com",
+          fileUrls: [
+            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
+          ]
+        },
+        {
+          telephone: "+237690346872",
+          email: "vladimirfomene@gmail.com",
+          fileUrls: [
+            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
+          ]
+        }
+      ],
+      isUploaded: false
+    };
   },
+  components: {
+    NavBarComponent,
+    UploadBarComponent,
+    SideBarNavComponent,
+    ResultMediaComponent,
+  }
 };
 </script>
