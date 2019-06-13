5b9842a1461ad6691c5d8e617ddb441bfc5223a2
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 14:59:07 +0100

    create transactionsearchcomponent

---
+++ src/components/TransactionSearchComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="level-right">
+    <div class="field has-addons">
+      <div class="control">
+        <span class="select">
+          <select>
+            <option>Search By</option>
+            <option>Email</option>
+            <option>Telephone</option>
+          </select>
+        </span>
+      </div>
+      <div class="control">
+        <input class="input" type="text" placeholder="Search transactions">
+      </div>
+      <div class="control">
+        <a class="button is-info">Search</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TransactionSearchComponent.vue"
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
+
