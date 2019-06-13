0a940e0167646a747f40b733810a390aa9b50e93
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 14:58:34 +0100

    create transactionfiltercomponent

---
+++ src/components/TransactionFilterComponent.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="level-right">
+    <div class="field">
+      <div class="select">
+        <select>
+          <option>Filter By</option>
+          <option>Failed Email</option>
+          <option>Failed SMS</option>
+        </select>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: "TransactionFilterComponent"
+};
+</script>
+
+
+<style lang="scss" scoped>
+</style>
