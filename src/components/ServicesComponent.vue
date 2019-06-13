cd9f883061107fb5a094415d565fd4a74a58f659
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 14:45:21 +0100

    shorten paragraphs in services component

--- src/components/ServicesComponent.vue
+++ src/components/ServicesComponent.vue
@@ -1,30 +1,39 @@
 <template>
   <div class="columns is-vcentered">
     <div class="column">
-      <h1 class="title has-text-centered">Medical Result <br>Delivery</h1>
-      <p class="has-text-centered">This service allows you to use Sms and Email to send your customers' medical exam results once they are ready.</p>
+      <h1 class="title has-text-centered">
+        Medical Result
+        <br>Delivery
+      </h1>
+      <p
+        class="has-text-centered"
+      >This service allows you to use Sms and Email to send your
+      customers' medical exam results once they are ready.</p>
     </div>
     <div class="column">
       <h1 class="title has-text-centered">Customer Payment Service</h1>
-      <p class="has-text-centered">This service allows you to accept electronic payment from your customers whatever payment method they are using.</p>
+      <p
+        class="has-text-centered"
+      >This service allows you to accept electronic payment from your
+      customers whatever payment method they are using.</p>
     </div>
     <div class="column">
       <h1 class="title has-text-centered">Customer Scheduling Service</h1>
-      <p class="has-text-centered">This service will give you the tools to schedule your customers whenever they come for a medical examination.</p>
+      <p
+        class="has-text-centered"
+      >This service will give you the tools to schedule your customers whenever
+      they come for a medical examination.</p>
     </div>
   </div>
 </template>
 
 
 <script>
-
 export default {
   name: 'ServicesComponent',
 };
-
 </script>
 
 
-<style>
-
+<style scoped lang="scss">
 </style>
