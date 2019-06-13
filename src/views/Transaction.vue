de894dde759c3ce29bf6dd951b3bdc6f7847ecf6
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 15:04:03 +0100

    build transaction view with search, table and filter components

---
+++ src/views/Transaction.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="transaction">
+    <NavBarComponent :isAuthenticated="isAuthenticated"/>
+    <div class="section">
+      <div class="columns">
+        <SideBarNavComponent />
+        <div class="column">
+          <div class="level">
+            <TransactionSearchComponent />
+            <TransactionFilterComponent />
+          </div>
+          <TransactionTableComponent />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+
+
+<script>
+import TransactionSearchComponent from "@/components/TransactionSearchComponent.vue";
+import TransactionFilterComponent from "@/components/TransactionFilterComponent.vue";
+import TransactionTableComponent from "@/components/TransactionTableComponent.vue";
+import SideBarNavComponent from "@/components/SideBarNavComponent.vue";
+import NavBarComponent from "@/components/NavBarComponent.vue";
+
+export default {
+  name: "Transaction",
+  data() {
+    return {
+      isAuthenticated: true
+    };
+  },
+  components: {
+    NavBarComponent,
+    SideBarNavComponent,
+    TransactionTableComponent,
+    TransactionFilterComponent,
+    TransactionSearchComponent,
+  }
+};
+</script>
+
+
+<style>
+</style>
