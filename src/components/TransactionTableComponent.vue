f41905997c46b02f2a2c23a6ece677dd1d2c897e
Vladimir Fomene <vladimirfomene@gmail.com>
2019-06-13 15:00:47 +0100

    create transactiontable component

---
+++ src/components/TransactionTableComponent.vue
@@ -0,0 +1,60 @@
+<template>
+  <table class="table is-hoverable is-fullwidth">
+    <thead>
+      <tr>
+        <th class="is-narrow">
+          <input type="checkbox">
+        </th>
+        <th>Email</th>
+        <th>Telephone</th>
+        <th>Email Status</th>
+        <th>Sms Status</th>
+        <th>Results</th>
+        <th>Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>
+          <input type="checkbox">
+        </td>
+        <td>
+          <code>+237690346872</code>
+        </td>
+        <td>
+          <code>vladimirfomene@gmail.com</code>
+        </td>
+        <td>
+          <span class="icon">
+            <i class="fas fa-check-circle"></i>
+          </span>
+        </td>
+        <td>
+          <span class="icon">
+            <i class="fas fa-exclamation-circle"></i>
+          </span>
+        </td>
+        <td>
+          <span class="icon">
+            <i class="fas fa-file-alt"></i>
+          </span>
+        </td>
+        <td>
+          <div class="buttons">
+            <a class="button is-small">Retry SMS</a>
+            <a class="button is-small">Retry Email</a>
+          </div>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+export default {
+  name: "TransactionTableComponent"
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
