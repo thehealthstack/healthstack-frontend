4e8e60b1bbe11fed2f3ee79409237d00c0dd1917
Vladimir Fomene <vladimirfomene@gmail.com>
2019-07-05 21:25:11 +0100

    add code to get medical results, display result components and uploadbarcomponent

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,8 +5,8 @@
       <div class="columns">
         <SideBarNavComponent />
         <div class="column">
-          <UploadBarComponent />
-          <ResultMediaComponent v-for="(result, index) in results" :result="result" :key="index"/>
+          <UploadBarComponent :medicalResults="medicalResults" />
+          <ResultMediaComponent v-for="(result, index) in medicalResults" :result="result" :key="index"/>
         </div>
       </div>
     </div>
@@ -14,47 +14,31 @@
 </template>
 
 <script>
-import ResultMediaComponent from "@/components/ResultMediaComponent.vue";
-import UploadBarComponent from "@/components/UploadBarComponent.vue";
-import SideBarNavComponent from "@/components/SideBarNavComponent.vue"
-import NavBarComponent from "@/components/NavBarComponent.vue";
+import ResultMediaComponent from '@/components/ResultMediaComponent.vue';
+import UploadBarComponent from '@/components/UploadBarComponent.vue';
+import SideBarNavComponent from '@/components/SideBarNavComponent.vue';
+import NavBarComponent from '@/components/NavBarComponent.vue';
+import { mapGetters } from 'vuex';
 
 export default {
-  name: "Home",
+  name: 'Home',
   data() {
     return {
       isAuthenticated: true,
-      results: [
-        {
-          telephone: "+237690346872",
-          email: "vladimirfomene@gmail.com",
-          fileUrls: [
-            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
-          ]
-        },
-        {
-          telephone: "+237690346872",
-          email: "vladimirfomene@gmail.com",
-          fileUrls: [
-            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
-          ]
-        },
-        {
-          telephone: "+237690346872",
-          email: "vladimirfomene@gmail.com",
-          fileUrls: [
-            "https://healthstack-centrepasteur-yaounde.s3-eu-west-1.amazonaws.com/MIT2_71S09_gps3.pdf"
-          ]
-        }
-      ],
-      isUploaded: false
+      isUploading: false,
     };
+  },
+  created(){
+    this.$store.dispatch('getMedicalResults');
+  },
+  computed: {
+    ...mapGetters(['medicalResults']),
   },
   components: {
     NavBarComponent,
     UploadBarComponent,
     SideBarNavComponent,
     ResultMediaComponent,
-  }
+  },
 };
 </script>
